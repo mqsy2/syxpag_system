07bb10666f37636682d705f7012bc4871ab164a6
mqsy2 <mqsy2@student.apc.edu.ph>
2025-03-06 17:52:58 +0000

    ffafas

---
+++ sxypag-order-system/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand fw-bold fst-italic text-light" href="#">SXYPAG</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Order</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Transactions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Supply</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.nav-link {
+  color: white !important;
+}
+.nav-link.active {
+  font-weight: bold;
+}
+</style>
---
+++ sxypag-order-system/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container text-center mt-5">
+    <h1 class="fw-bold">Welcome to SXYPAG Order System</h1>
+    <p class="text-muted">
+      Manage orders, track transactions, monitor supply, and browse products efficiently.
+    </p>
+  </div>
+</template>
